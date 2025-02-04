cf45dcd4c697167ffe1b6d2908588e07d1479190
Ignacio Marenco <ignaciomarencodls@gmail.com>
2025-02-04 01:09:07 -0300

    week 4

--- routes/user.js
+++ routes/user.js
@@ -5,9 +5,9 @@ const commentController = require("../controllers/user");
 const Auth = require("../helpers/auth");
 const { getUserValidations } = require("../middleware/validate");
 
-router.get("/", Auth.login, commentController.getAll);
+router.get("/", commentController.getAll);
 
-router.get("/:id", commentController.getSingle);
+router.get("/:id",  Auth.login, commentController.getSingle);
 
 router.post("/", getUserValidations(), commentController.createUser);
 
